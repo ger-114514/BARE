ccb24484c9b294fd3229d2557273aa018bacfc7b
常智祺 <1064545197@qq.com>
2025-04-27 16:24:00 +0800

    114514

--- common/defines/EDO_defines.lua
+++ common/defines/EDO_defines.lua
@@ -5,8 +5,8 @@ NDefines.NGame.END_DATE = "2200.1.1.1"
 NDefines.NProduction.MAX_EQUIPMENT_RESOURCES_NEED = 5	-- Max number of different strategic resources an equipment can be dependent on. 包含重工和欧帕兹
 NDefines.NGame.FUEL_RESOURCE = "resources_fe"						-- resource that will give country fuel
 
-NDefines.NFrontend.CAMERA_MIN_HEIGHT = 50.0
-NDefines.NFrontend.CAMERA_MAX_HEIGHT = 900.0
+NDefines.NFrontend.CAMERA_MIN_HEIGHT = 25.0
+NDefines.NFrontend.CAMERA_MAX_HEIGHT = 3000.0
 NDefines.NGraphics.PROVINCE_NAME_DRAW_DISTANCE = 500.0 			-- Remove province names beyond this distance
 
 NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 3.25
